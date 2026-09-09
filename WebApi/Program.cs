260077dee5aac1814db6ca21cf9ff11969c53a21
Gareth Spencer <gareth.spencer1@hotmail.co.uk>
2026-09-09 18:56:38 +0100

    Setting up openApi with a document transformer to add authentication to the endpoints in the swagger UI

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
+using WebApi.Transformers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,13 +8,22 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer<OAuthSecuritySchemeTransformer>();
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "RunBook API v1");
+        options.OAuthClientId(builder.Configuration["AzureAd:ClientId"]);
+        options.OAuthUsePkce();
+    });
 }
 
 app.UseHttpsRedirection();
---
+++ WebApi/Transformers/OAuthSecuritySchemeTransformer.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi;
+
+namespace WebApi.Transformers
+{
+    public class OAuthSecuritySchemeTransformer(IConfiguration configuration) : IOpenApiDocumentTransformer
+    {
+        public Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
+        {
+            var scope = "api://" + configuration["AzureAd:ClientId"] + "/access_as_user";
+
+            document.Components ??= new();
+            document.Components.SecuritySchemes ??= new Dictionary<string, IOpenApiSecurityScheme>();
+            document.Components.SecuritySchemes["oauth2"] = new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.OAuth2,
+                Flows = new OpenApiOAuthFlows
+                {
+                    AuthorizationCode = new OpenApiOAuthFlow
+                    {
+                        AuthorizationUrl = new Uri("https://login.microsoftonline.com/" + configuration["AzureAd:TenantId"] + "/oauth2/v2.0/authorize"),
+                        TokenUrl = new Uri("https://login.microsoftonline.com/" + configuration["AzureAd:TenantId"] + "/oauth2/v2.0/token"),
+                        Scopes = new Dictionary<string, string>
+                        {
+                            [scope] = "Access WebApi as user"
+                        }
+                    }
+                }
+            };
+
+            foreach (var operation in document.Paths.Values.SelectMany(p => p.Operations?.Values ?? Enumerable.Empty<OpenApiOperation>()))
+            {
+                operation.Security ??= [];
+                operation.Security.Add(new OpenApiSecurityRequirement
+                {
+                    [new OpenApiSecuritySchemeReference("oauth2", document)] = [scope]
+                });
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
