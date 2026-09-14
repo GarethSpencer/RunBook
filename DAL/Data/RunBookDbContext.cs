5aa50495d81466e813033a27c9395914608d8d06
Gareth Spencer <gareth.spencer1@hotmail.co.uk>
2026-09-14 18:58:35 +0100

    Adding DAL layer with basic DbContext structure

---
+++ DAL/Data/RunBookDbContext.cs
@@ -0,0 +1,45 @@
+﻿using DAL.Data.Configuration;
+using DAL.Entities.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Data;
+
+public class RunBookDbContext(DbContextOptions<RunBookDbContext> options) : DbContext(options)
+{
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(RunBookDbContext).Assembly);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        SetAuditFields(SeedConstants.AdminId);
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<int> SaveChangesAsync(Guid userId, CancellationToken cancellationToken = default)
+    {
+        SetAuditFields(userId);
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+
+    private void SetAuditFields(Guid userId)
+    {
+        var now = DateTime.UtcNow;
+
+        foreach (var entry in ChangeTracker.Entries<IAuditableEntity>().Where(e => e.State == EntityState.Added).ToList())
+        {
+            entry.Entity.CreatedOn = now;
+            entry.Entity.CreatedBy = userId;
+        }
+
+        foreach (var entry in ChangeTracker.Entries<IAuditableEntity>().Where(e => e.State == EntityState.Modified).ToList())
+        {
+            entry.Entity.UpdatedOn = now;
+            entry.Entity.UpdatedBy = userId;
+        }
+    }
+}
